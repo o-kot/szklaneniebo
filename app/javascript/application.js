2d48c166d17f28cb06767661fb73471ba61aa233
Oki <okoczek@gmail.com>
2024-06-21 17:17:42 +0300

    Initial layout for the homepage
    Finished layout version 1 for the index page
    
    Finished layout version 1 for the gallery page

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,33 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+$(document).ready(function() {
+  $('[data-fancybox]').fancybox({
+    loop: true, // Allows navigation in a loop
+    buttons: [
+      "zoom",
+      "slideShow",
+      "fullScreen",
+      "thumbs",
+      "close"
+    ],
+    protect: true,
+    animationEffect: "zoom",
+    transitionEffect: "fade",
+    transitionDuration: 500,
+    smallBtn: "auto",
+    toolbar: "auto",
+    image: {
+      preload: true
+    },
+    afterLoad: function(instance, current) {
+      // Blur the background
+      $('body').addClass('blurred');
+    },
+    afterClose: function(instance, current) {
+      // Remove blur from background
+      $('body').removeClass('blurred');
+    }
+  });
+});
