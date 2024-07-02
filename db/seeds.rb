18b1815fc93c75d2d8bd42420118b939390deea3
Oki <okoczek@gmail.com>
2024-07-02 13:25:05 +0300

    Moving rest of the artist's info to the storage

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,8 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+Author.create(
+  name: 'Alicja Zapolska',
+  about: "Fascynacja witrażami była we mnie od zawsze.\nObraz, który się zmienia. Gra światła do słonecznej muzyki.\nKiedyś byłam przekonana, że witraże to hobby dla nielicznych.\nI tak jest - wymaga cierpliwości, pokory, akceptacji, niespieszności.\nSzkło - kruche i twarde, zimne i ciepłe, delikatne i ostre...\nZauroczyło mnie i wypełniło wolne chwile.\nWitrażowe hobby trwa już kilka lat, pracuję w technice Tiffaniego.\nMoje prace goszczą w wielu domach, zarówno w Polsce jak i za granicą.\nKONTAKT - alicja-631@wp.pl\nDziękuję za odwiedzenie mej strony..."
+)
