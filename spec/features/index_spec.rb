487a7a4ce82c1ef9e557a89e34f3aba6975031be
Oki <okoczek@gmail.com>
2024-07-07 16:17:30 +0300

    Content translation for admin dashboard

--- spec/features/index_spec.rb
+++ spec/features/index_spec.rb
@@ -2,10 +2,11 @@ require 'rails_helper'
 
 RSpec.feature "IndexPage", type: :feature do
   before do
-    create(:author)
+    create(:author, about: "Fascynacja witrażami była we mnie od zawsze.")
+    I18n.backend.store_translations(:en, { about: { text: "I've always been fascinated by stained glass." } })
   end
 
-  scenario "renders index page with expected content" do
+  scenario "renders index page with expected content in default locale" do
     visit root_path
     expect(page).to have_content('Fascynacja witrażami była we mnie od zawsze.')
   end
@@ -23,5 +24,10 @@ RSpec.feature "IndexPage", type: :feature do
   scenario "loads the artist's photo" do
     visit root_path
     expect(page).to have_css("img[alt='Artist portrait']")
+  end
+
+  scenario "renders index page with expected content in English locale" do
+    visit root_path(locale: :en)
+    expect(page).to have_content("I've always been fascinated by stained glass.")
   end
 end
