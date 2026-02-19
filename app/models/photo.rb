48d12cfb43c6aea712f20e6b0ee3e805a6f0f641
Ana Wolak <theanawolak@gmail.com>
2026-02-19 14:28:28 +0100

    fixes photo upload

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -3,7 +3,6 @@ class Photo < ApplicationRecord
   has_one_attached :image
 
   validate :unique_photo_in_category
-  after_commit :resize_image, on: [:create, :update]
 
   MAX_IMAGE_DIMENSION = 2000
   IMAGE_QUALITY = 85
@@ -18,42 +17,5 @@ class Photo < ApplicationRecord
     return unless existing_photo
 
     errors.add(:image, 'has already been added to this category')
-  end
-
-  def resize_image
-    return unless image.attached?
-    return unless image.blob.content_type.start_with?('image/')
-
-    require 'image_processing/vips'
-
-    begin
-      resized_image = ImageProcessing::Vips
-        .source(image.blob.download)
-        .resize_to_limit(MAX_IMAGE_DIMENSION, MAX_IMAGE_DIMENSION)
-        .saver(quality: IMAGE_QUALITY)
-        .call
-
-      image.attach(
-        io: File.open(resized_image.path),
-        filename: image.blob.filename,
-        content_type: image.blob.content_type
-      )
-    rescue LoadError
-      # Fall back to ImageMagick if Vips is not available
-      resized_image = ImageProcessing::MiniMagick
-        .source(image.blob.download)
-        .resize_to_limit(MAX_IMAGE_DIMENSION, MAX_IMAGE_DIMENSION)
-        .convert('jpg')
-        .saver(quality: IMAGE_QUALITY)
-        .call
-
-      image.attach(
-        io: File.open(resized_image.path),
-        filename: image.blob.filename,
-        content_type: image.blob.content_type
-      )
-    ensure
-      resized_image&.unlink if resized_image.respond_to?(:unlink)
-    end
   end
 end
